97707a47879519c36872c591a016526a01969902
Pierre Locus <pierre.locus@relatient.net>
2022-04-02 21:34:28 +0200

    [FIX] Events in calendar

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from database import Database
 import json
+import datetime
 api = Flask(__name__)
 
 NULL = 0
@@ -19,7 +20,11 @@ def admin():
 @api.route('/calendar', methods=['GET'])
 def get_events():
     # Chercher en db les events
-    return render_template('calendar.html')
+    return render_template('calendar.html', events=[{
+		'title': 'Hello',
+		'start': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+		'end': (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime('%Y-%m-%d %H:%M:%S')
+	}])
 
 @api.route('/cursusflix', methods=['GET'])
 def get_cursusflix():
@@ -47,29 +52,32 @@ def get_profile():
 # =================================================================
 @api.route('/calendar/events/list/all')
 def get_all_events():
-    return {
-		"id": 1,
-		"view_group": "admin",
-		"ownerId": 1,
-		"allDay": 0,
-		"start": "2022-04-02 10:00:00",
-		"startStr": "",
-		"end": "2022-04-02 20:00:00",
-		"endStr": "",
-		"title": "Fbqzfbqlzkbc",
-		"url": "http://localhost:8080/calendar/event/view?id=1",
-		"editable": NULL,
-		"startEditable": NULL,
-		"durationEditable": NULL,
-		"resourceEditable": NULL,
-		"display": "auto",
-		"overlap": NULL,
-		"constraint": "",
-		"backgroundColor": "#000000",
-		"borderColor": "#000000",
-		"textColor": "#000000",
-		"source": "http://localhost:8080/calendar/event/getevent?id=1"
-	}
+	return {'events': [{
+		'title': 'Hello',
+		'start': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+		'end': (datetime.datetime.now() + datetime.timedelta(hours=2)).strftime('%Y-%m-%d %H:%M:%S')
+	}]}
+    #return jsonify({
+	#	"id": 1,
+	#	"view_group": "admin",
+	#	"ownerId": 1,
+	#	"allDay": 0,
+	#	"start": "2022-04-02 10:00:00",
+	#	"end": "2022-04-02 20:00:00",
+	#	"title": "Fbqzfbqlzkbc",
+	#	"url": "http://localhost:8080/calendar/event/view?id=1",
+	#	"editable": NULL,
+	#	"startEditable": NULL,
+	#	"durationEditable": NULL,
+	#	"resourceEditable": NULL,
+	#	"display": "auto",
+	#	"overlap": NULL,
+	#	"constraint": "",
+	#	"backgroundColor": "#000000",
+	#	"borderColor": "#000000",
+	#	"textColor": "#000000",
+	#	"source": "http://localhost:8080/calendar/event/getevent?id=1"
+	#})
 
 if __name__ == '__main__':
     api.run() 
