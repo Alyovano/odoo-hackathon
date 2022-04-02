b730a93ab603e38f44f999a9f8bcba7950062268
alyovano <alyovaski@gmail.com>
2022-04-02 15:09:00 +0200

    Rebuild -> template

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ api = Flask(__name__)
 
 @api.route('/', methods=['GET'])
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @api.route('/events', methods=['GET'])
 def get_events():
